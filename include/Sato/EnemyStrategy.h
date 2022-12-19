e254ef739ecaee5823aaaf2c5a3d2e4c9c55aff7
Dylan Ascencio <empressglatia@gmail.com>
2022-12-19 01:14:29 -0500

    Starting to integrate Ninja as a build system

---
+++ include/Sato/EnemyStrategy.h
@@ -0,0 +1,48 @@
+#pragma once
+
+typedef unsigned long size_t;
+
+class JORReflexible
+{
+  
+};
+
+class EnemyStrategy : public JORReflexible
+{
+public:  
+  EnemyStrategy();
+  virtual ~EnemyStrategy();
+  
+  virtual void init();
+  virtual void update();
+  virtual bool FUN_800c1314();
+  virtual void FUN_800c231c();
+  virtual void doBehavior();
+  virtual void doBehaviorInit();
+  
+  void changeState();
+  void setNextState(short nextState);
+  
+  static void* operator new(size_t size, void* mem) { return mem; }
+  static void operator delete(void* mem) { }
+  
+  void* mpEnZako;
+  void* mpUserData;
+  short mNextState;
+  short mCurrentState;
+  unsigned int mTimer;
+};
+
+class EnemyStrategyDecorator : public EnemyStrategy
+{
+  EnemyStrategy* mpTsuriStrategy;
+  unsigned short mTsuriState;
+  
+public:
+  EnemyStrategyDecorator();
+  virtual ~EnemyStrategyDecorator();
+  
+  virtual void update();
+  
+  bool trySetTsuriStrategy(EnemyStrategy* tsuriStrategy);
+};
---
+++ src/Sato/EnemyStrategy.cpp
@@ -0,0 +1,125 @@
+#include "EnemyStrategy.h"
+
+/* ===== EnemyStrategy ===== */
+
+EnemyStrategy::EnemyStrategy()
+{
+  mpEnZako = 0;
+  mpUserData = 0;
+  mNextState = 0;
+  mCurrentState = 0;
+  mTimer = 0;
+}
+
+EnemyStrategy::~EnemyStrategy()
+{
+  
+}
+
+void EnemyStrategy::init()
+{
+  
+}
+
+void EnemyStrategy::update()
+{
+  if (mNextState != -1)
+  {
+    changeState();
+  }
+  
+  doBehavior();
+  mTimer++;
+}
+
+void EnemyStrategy::doBehavior()
+{
+  
+}
+
+void EnemyStrategy::doBehaviorInit()
+{
+  
+}
+
+bool EnemyStrategy::FUN_800c1314()
+{
+  return false;
+}
+
+void EnemyStrategy::FUN_800c231c()
+{
+  
+}
+  
+void EnemyStrategy::setNextState(short nextState)
+{
+  mNextState = nextState;
+}
+
+void EnemyStrategy::changeState()
+{
+  mCurrentState = mNextState;
+  mNextState = -1;
+  mTimer = 0;
+  
+  doBehaviorInit();
+}
+
+/* ===== EnemyStrategyDecorator ===== */
+
+EnemyStrategyDecorator::EnemyStrategyDecorator()
+{
+  mpEnZako = 0;
+  mpUserData = 0;
+  mNextState = 0;
+  mCurrentState = 0;
+  mTimer = 0;
+  
+  mpTsuriStrategy = 0;
+  mTsuriState = 0x100;
+}
+
+EnemyStrategyDecorator::~EnemyStrategyDecorator()
+{
+  
+}
+  
+void EnemyStrategyDecorator::update()
+{
+  if (mpTsuriStrategy == 0)
+  {
+    if (mNextState != -1)
+    {
+      mCurrentState = mNextState;
+      mNextState = -1;
+      mTimer = 0;
+      doBehaviorInit();
+    }
+    
+    doBehavior();
+    mTimer++;
+  }
+  else
+  {
+    mpTsuriStrategy->update();
+    
+    unsigned short tsuriState = mpTsuriStrategy->mCurrentState;
+    if (tsuriState > 0xFF && tsuriState < 0x103)
+    {
+      mTsuriState = tsuriState;
+      mpTsuriStrategy = 0;
+    }
+  }
+}
+  
+bool EnemyStrategyDecorator::trySetTsuriStrategy(EnemyStrategy* tsuriStrategy)
+{
+  if (mpTsuriStrategy != 0)
+  {
+    return false;
+  }
+  
+  mpTsuriStrategy = tsuriStrategy;
+  return true;
+}
