5b8b5a36a9be489f5e42c701b6cf138885fac0e9
EpochFlame <jupiterror@gmail.com>
2023-07-11 20:36:31 -0400

    restructure the build system
    now we're in line with pikmin

---
+++ asm/dtors.s
@@ -0,0 +1,17 @@
+.include "macros.s"
+
+.section .dtors, "wa"  # 0x8020BDE0 - 0x8020BE00 ; 0x00000020
+
+
+.global lbl_8020BDE0
+lbl_8020BDE0:
+
+	# ROM: 0x208DE0
+	.4byte lbl_80200188
+	.4byte lbl_80202664
+	.4byte lbl_80200188
+	.4byte 0
+	.4byte 0
+	.4byte 0
+	.4byte 0
+	.4byte 0
---
+++ asm/extab_.s
@@ -0,0 +1,200 @@
+.include "macros.s"
+
+.section extab_, "wa"  # 0x800054C0 - 0x80005640 ; 0x00000180
+
+
+.global lbl_800054C0
+lbl_800054C0:
+
+	# ROM: 0x208820
+	.4byte 0x10180000
+	.4byte 0x0000003C
+	.4byte 0x00000018
+	.4byte 0x0000004C
+	.4byte 0x00000024
+	.4byte 0
+	.4byte 0x8F000000
+	.4byte 0x00000044
+	.4byte 0x00000010
+	.4byte 0x8D000010
+
+.global lbl_800054E8
+lbl_800054E8:
+
+	# ROM: 0x208848
+	.4byte 0x30080000
+	.4byte 0
+
+.global lbl_800054F0
+lbl_800054F0:
+
+	# ROM: 0x208850
+	.4byte 0x20080000
+	.4byte 0
+
+.global lbl_800054F8
+lbl_800054F8:
+
+	# ROM: 0x208858
+	.4byte 0x20080000
+	.4byte 0x00000064
+	.4byte 0x00000010
+	.4byte 0
+	.4byte 0x8200001C
+	.4byte lbl_80200454
+
+.global lbl_80005510
+lbl_80005510:
+
+	# ROM: 0x208870
+	.4byte 0x18080000
+	.4byte 0
+
+.global lbl_80005518
+lbl_80005518:
+
+	# ROM: 0x208878
+	.4byte 0x28080000
+	.4byte 0x00000074
+	.4byte 0x00000010
+	.4byte 0
+	.4byte 0x8200001C
+	.4byte lbl_80200454
+
+.global lbl_80005530
+lbl_80005530:
+
+	# ROM: 0x208890
+	.4byte 0x00080000
+	.4byte 0
+
+.global lbl_80005538
+lbl_80005538:
+
+	# ROM: 0x208898
+	.4byte 0x00080000
+	.4byte 0
+
+.global lbl_80005540
+lbl_80005540:
+
+	# ROM: 0x2088A0
+	.4byte 0x00080000
+	.4byte 0
+
+.global lbl_80005548
+lbl_80005548:
+
+	# ROM: 0x2088A8
+	.4byte 0x00080000
+	.4byte 0
+
+.global lbl_80005550
+lbl_80005550:
+
+	# ROM: 0x2088B0
+	.4byte 0x08080000
+	.4byte 0
+
+.global lbl_80005558
+lbl_80005558:
+
+	# ROM: 0x2088B8
+	.4byte 0x10180000
+	.4byte 0x0000004C
+	.4byte 0x00000018
+	.4byte 0x0000005C
+	.4byte 0x00000024
+	.4byte 0
+	.4byte 0x8F000000
+	.4byte 0x00000054
+	.4byte 0x00000028
+	.4byte 0x8D000028
+
+.global lbl_80005580
+lbl_80005580:
+
+	# ROM: 0x2088E0
+	.4byte 0x10180000
+	.4byte 0x0000004C
+	.4byte 0x00000018
+	.4byte 0x0000005C
+	.4byte 0x00000024
+	.4byte 0
+	.4byte 0x8F000000
+	.4byte 0x00000054
+	.4byte 0x00000028
+	.4byte 0x8D000028
+
+.global lbl_800055A8
+lbl_800055A8:
+
+	# ROM: 0x208908
+	.4byte 0x00080000
+	.4byte 0
+
+.global lbl_800055B0
+lbl_800055B0:
+
+	# ROM: 0x208910
+	.4byte 0x30080000
+	.4byte 0x000002A8
+	.4byte 0x00100010
+	.4byte 0
+	.4byte 0x8E000000
+
+.global lbl_800055C4
+lbl_800055C4:
+
+	# ROM: 0x208924
+	.4byte 0x30180000
+	.4byte 0x00000028
+	.4byte 0x00000018
+	.4byte 0x00000050
+	.4byte 0x00510028
+	.4byte 0
+	.4byte 0x90000105
+	.4byte 0
+	.4byte 0x0000002C
+	.4byte 0x00000058
+	.4byte 0x8D000058
+
+.global lbl_800055F0
+lbl_800055F0:
+
+	# ROM: 0x208950
+	.4byte 0x10180000
+	.4byte 0x00000054
+	.4byte 0x00000018
+	.4byte 0x00000064
+	.4byte 0x00000024
+	.4byte 0
+	.4byte 0x8F000000
+	.4byte 0x0000005C
+	.4byte 0x00000028
+	.4byte 0x8D000028
+
+.global lbl_80005618
+lbl_80005618:
+
+	# ROM: 0x208978
+	.4byte 0x48080000
+	.4byte 0x00000044
+	.4byte 0x01410010
+	.4byte 0
+	.4byte 0x8E000000
+
+.global lbl_8000562C
+lbl_8000562C:
+
+	# ROM: 0x20898C
+	.4byte 0x10080000
+	.4byte 0
+
+.global lbl_80005634
+lbl_80005634:
+
+	# ROM: 0x208994
+	.4byte 0x10080000
+	.4byte 0
+	.4byte 0
